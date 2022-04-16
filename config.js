40bcce71e7cd9df0d4fdf18d22c5c0cf3e7dd3c5
santhosh kumar <75415950+Santhoshkumar27@users.noreply.github.com>
2022-04-16 11:10:01 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCAkY6KguWasm-2npRZ_TPuEi-d97qCGko",
+    authDomain: "postgram-a502c.firebaseapp.com",
+    projectId: "postgram-a502c",
+    storageBucket: "postgram-a502c.appspot.com",
+    messagingSenderId: "176051593663",
+    appId: "1:176051593663:web:eaaeef678c727dab98810a",
+    measurementId: "G-89TSMJEN20"
+  };
